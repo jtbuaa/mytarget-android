3b5016d8216ec896af64e097fcaa466a2df4603f
taoj <taoj@opera.com>
2017-12-09 12:00:13 +0800

    Override startActivityForResult to open ads with ourown's method

--- myTargetDemo/app/src/main/java/com/my/targetDemoApp/activities/NativeAdActivity.java
+++ myTargetDemo/app/src/main/java/com/my/targetDemoApp/activities/NativeAdActivity.java
@@ -1,14 +1,18 @@
 package com.my.targetDemoApp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.URLUtil;
 
 import com.my.targetDemoApp.R;
 import com.my.targetDemoApp.fragments.NativeAdFragment;
@@ -18,6 +22,7 @@ import java.util.ArrayList;
 public class NativeAdActivity extends AdActivity
 		implements BottomNavigationView.OnNavigationItemSelectedListener
 {
+	public static final String TAG = NativeAdActivity.class.getSimpleName();
 	private static final String TAG_STATIC_FRAGMENT = "fragment_static";
 	private static final String TAG_VIDEO_FRAGMENT = "fragment_video";
 	private static final String TAG_SLIDER_FRAGMENT = "fragment_slider";
@@ -96,5 +101,56 @@ public class NativeAdActivity extends AdActivity
 					.commit();
 		}
 		return true;
+	}
+
+	/**
+	 * Overridden to intercept any intent requests from third-party libs (such as MyTarget SDK)
+	 * to check if we could handle them ourselves directly. Overriding this method since the
+	 * default implementation of the other flavors of startActivity[ForResult] is to eventually
+	 * end up calling this method.
+	 */
+	@Override
+	public void startActivityForResult(Intent intent, int requestCode, @Nullable Bundle options) {
+		if (handleIntent(intent)) return;
+		super.startActivityForResult(intent, requestCode, options);
+	}
+
+	private boolean handleIntent(@Nullable final Intent intent) {
+		if (intent == null) return false;
+		if (intent.getCategories() == null) {
+			// Intercept ad related intents and open them inside our browser when possible
+			if (Intent.ACTION_VIEW.equals(intent.getAction()) && intent.getDataString() != null
+					&& intent.getComponent() == null) {
+				final String url = intent.getDataString();
+				if (URLUtil.isNetworkUrl(url)) {
+					openInternal(url);
+					return true;
+				}
+			}
+			return false;
+		}
+		boolean browsable = false;
+		for (String category : intent.getCategories()) {
+			if (Intent.CATEGORY_BROWSABLE.equals(category)) {
+				browsable = true;
+				break;
+			}
+		}
+		if (browsable
+				&& Intent.ACTION_VIEW.equals(intent.getAction())
+				&& intent.getDataString() != null
+				&& intent.getComponent() == null) {
+			final String url = intent.getDataString();
+			if (URLUtil.isNetworkUrl(url)) {
+				openInternal(url);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void openInternal(final @NonNull String url) {
+		// Todo: open with our webview
+		Log.i(TAG, "openInternal " + url);
 	}
 }
